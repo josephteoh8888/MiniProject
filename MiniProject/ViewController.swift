7122887e943440e050d5549c6c8e2c23d2a8b689
Teoh Guan Hoe <josephteoh8888@gmail.com>
2024-06-28 16:09:53 +0800

    hide API key

--- MiniProject/ViewController.swift
+++ MiniProject/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        GMSServices.provideAPIKey("XXX") //replace with real API key
+        GMSServices.provideAPIKey("XXX") //replace with real API key
         let options = GMSMapViewOptions()
         options.camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         options.frame = self.view.bounds
